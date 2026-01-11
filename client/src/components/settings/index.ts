b2959ea7c899f985f59697215a680b9051909829
Hung150 <55206412+Hung150@users.noreply.github.com>
2026-01-11 17:35:39 +0300

    Update index.ts

--- client/src/components/settings/index.ts
+++ client/src/components/settings/index.ts
@@ -1,5 +1,5 @@
-// client/src/components/settings/index.ts
 export { default as AccountTab } from './AccountTab';
 export { default as ProfileTab } from './ProfileTab';
 export { default as PreferencesTab } from './PreferencesTab';
 export { default as NotificationsTab } from './NotificationsTab';
+export { default as TeamTab } from './TeamTab';
