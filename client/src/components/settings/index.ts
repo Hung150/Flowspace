733f294f3f17e7b628732632217d50627a7e57f2
Hung150 <55206412+Hung150@users.noreply.github.com>
2026-01-11 16:30:17 +0300

    Create index.ts

---
+++ client/src/components/settings/index.ts
@@ -0,0 +1,5 @@
+// client/src/components/settings/index.ts
+export { default as AccountTab } from './AccountTab';
+export { default as ProfileTab } from './ProfileTab';
+export { default as PreferencesTab } from './PreferencesTab';
+export { default as NotificationsTab } from './NotificationsTab';
